b6ffc6fe86665d549a3af992839deca31d5832b8
CrispoPlay <corellana.dieguez@gmail.com>
2026-05-06 22:59:58 -0600

    Agregando historial

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-mod calculadora; // importamos el módulo
+mod calculadora;
 
 use calculadora::Calculadora;
 use std::io;
 
-// función auxiliar
+// Función para leer números
 fn leer_numero() -> f64 {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Error");
@@ -11,22 +11,37 @@ fn leer_numero() -> f64 {
 }
 
 fn main() {
+    let mut historial: Vec<String> = Vec::new();
+
     loop {
         println!("\n--- CALCULADORA ---");
         println!("1. Sumar");
         println!("2. Restar");
         println!("3. Multiplicar");
         println!("4. Dividir");
-        println!("5. Salir");
+        println!("5. Ver historial");
+        println!("6. Salir");
 
         let mut opcion = String::new();
         io::stdin().read_line(&mut opcion).expect("Error");
 
         let opcion = opcion.trim();
 
-        if opcion == "5" {
-            println!("Gracias por usar el programa");
+        if opcion == "6" {
+            println!("Adiós 👋");
             break;
+        }
+
+        if opcion == "5" {
+            println!("--- Historial ---");
+            if historial.is_empty() {
+                println!("No hay operaciones aún");
+            } else {
+                for operacion in &historial {
+                    println!("{}", operacion);
+                }
+            }
+            continue;
         }
 
         println!("Ingrese el primer número:");
@@ -35,11 +50,11 @@ fn main() {
         println!("Ingrese el segundo número:");
         let b = leer_numero();
 
-        let resultado = match opcion {
-            "1" => Calculadora::sumar(a, b),
-            "2" => Calculadora::restar(a, b),
-            "3" => Calculadora::multiplicar(a, b),
-            "4" => Calculadora::dividir(a, b),
+        let (resultado, simbolo) = match opcion {
+            "1" => (Calculadora::sumar(a, b), "+"),
+            "2" => (Calculadora::restar(a, b), "-"),
+            "3" => (Calculadora::multiplicar(a, b), "*"),
+            "4" => (Calculadora::dividir(a, b), "/"),
             _ => {
                 println!("Opción inválida");
                 continue;
@@ -47,5 +62,8 @@ fn main() {
         };
 
         println!("Resultado: {}", resultado);
+
+        // Guardar en historial
+        historial.push(format!("{} {} {} = {}", a, simbolo, b, resultado));
     }
 }
